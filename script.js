3d916033b208df2c34a92fab5a2c4c1d258bb68d
Ravneet Kaur <neet.k962@gmail.com>
2023-09-27 10:28:24 -0700

    //added differnt API becausegoogleoauthnot working

--- script.js
+++ script.js
@@ -23,8 +23,30 @@ function signup(){
 
 
 }
-
-
+var access="93bccf21e00692f2ca8c6938ae0b90294a8d66851229c143d9526118676a8d76"
+var fileInput= document.getElementById('identifyform')
+var submitbutton=document.getElementById('submitbutton')
+submitbutton.addEventListener('click', function(event) {
+event.preventDefault()
+var myHeaders = new Headers();
+	myHeaders.append("accessKey", access);
+	
+	var formdata = new FormData();
+	formdata.append("image", fileInput.files[0]);
+	
+	var requestOptions = {
+	  method: 'POST',
+	  headers: myHeaders,
+	  body: formdata,
+	  redirect: 'follow'
+	};
+	
+	fetch("https://api.plantcaregarden.com/v1/plant/identify", requestOptions)
+	  .then(response => response.text())
+	  .then(result => console.log(result))
+	  .catch(error => console.log('error', error));
+}
+)
 
 
 
